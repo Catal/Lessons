2ff6579b86dcf87eca6d7d3e8ed5639d17b459ee
tmutocatal <tmutopop@gmail.com>
2014-10-02 20:18:22 +0900

    added image

--- catalengineer2014/config/routes.rb
+++ catalengineer2014/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  resources :notes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
