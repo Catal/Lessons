bb60c027ef2e0f532a92393e3fdd70f0ff146198
tmutocatal <tmutopop@gmail.com>
2014-10-02 16:41:25 +0900

    added picture

--- catalengineer2014/app/controllers/notes_controller.rb
+++ catalengineer2014/app/controllers/notes_controller.rb
@@ -2,4 +2,18 @@ class NotesController < ApplicationController
   def new
     @note = Note.new  
   end
+
+  def create
+    @note = Note.new(note_params)
+    if @note.save
+     redirect_to @note
+    else
+      render :new
+    end
+  end
+
+  def note_params
+    params.require(:note).permit(:title, :body, :date)
+  end
+  
 end
