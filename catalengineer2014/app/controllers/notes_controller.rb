1e7288dbc9a8c806d72cfa50bad197f67c354339
tmutocatal <tmutopop@gmail.com>
2014-10-02 18:31:57 +0900

    added picture

--- catalengineer2014/app/controllers/notes_controller.rb
+++ catalengineer2014/app/controllers/notes_controller.rb
@@ -1,9 +1,13 @@
 class NotesController < ApplicationController
 
+  def index 
+    @notes = Note.all
+  end
+
   def show
     @note = Note.find(params[:id])
   end
-  
+
   def new
     @note = Note.new  
   end
