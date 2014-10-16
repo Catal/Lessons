82406950ae7696ddc01647841cf626cb2c7d9f1d
Takuya Uehara <indigo.lain@gmail.com>
2014-10-16 16:54:02 +0900

    delete action added

--- catalengineer2014/app/controllers/notes_controller.rb
+++ catalengineer2014/app/controllers/notes_controller.rb
@@ -34,6 +34,12 @@ class NotesController < ApplicationController
     end
   end
 
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    redirect_to notes_path
+  end
+
   def note_params
     params.require(:note).permit(:title, :body, :date)
   end
