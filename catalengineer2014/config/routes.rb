9cd4f2cf98095763d09efea8c591267cd4beef75
tmutocatal <tmutopop@gmail.com>
2014-10-23 14:02:07 +0900

    updated image

--- catalengineer2014/config/routes.rb
+++ catalengineer2014/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  #root :to => "notes#index"
+  devise_for :users
+  root :to => "notes#index"
   resources :notes
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
