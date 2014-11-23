454be35a34f4adf5e31483866f0a2fe9f8d97f59
tmutocatal <tmutopop@gmail.com>
2014-11-23 22:03:47 +0900

    updated

--- sensenoteclone/config/routes.rb
+++ sensenoteclone/config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  routes : 'notes#index'
+  
+  get '/notes/takeshi', to: 'notes#takeshi'
+
+  root :to => "notes#index"
+
   resources :profiles
 
   resources :groups
