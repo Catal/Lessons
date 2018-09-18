6e009ab804cb5e53fb76512e1832a1d5efa47aea
Masumi Kawasaki <mkawasaki@sbihg.co.jp>
2018-09-18 13:03:11 +0900

    Update gems

--- sensenoteclone/db/schema.rb
+++ sensenoteclone/db/schema.rb
@@ -13,20 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20141212042220) do
 
-  create_table "group_users", force: true do |t|
+  create_table "group_users", force: :cascade do |t|
     t.integer  "group_id"
     t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "groups", force: true do |t|
+  create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "notes", force: true do |t|
+  create_table "notes", force: :cascade do |t|
     t.string   "title"
     t.string   "body"
     t.datetime "date"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20141212042220) do
     t.datetime "updated_at"
   end
 
-  create_table "profiles", force: true do |t|
+  create_table "profiles", force: :cascade do |t|
     t.string   "name"
     t.string   "avatar"
     t.datetime "created_at"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20141212042220) do
     t.integer  "user_id"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
